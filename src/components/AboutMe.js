c3b2d2d99f1fce184e8bd76ba159dcfc0e316640
Cole Buck <67663402+ColeBuck@users.noreply.github.com>
2021-10-26 12:10:48 -0400

    fix typo

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -3,7 +3,7 @@ import "./AboutMe.scss"
 
 export default function AboutMe() { 
     return <div>
-        <p id="top">Hey there, and welcome to my portfolio site! Explore the links above to see some of the things that I've been up to! Or, keep reading below to get to know me a litte better! </p>
+        <p id="top">Hey there, and welcome to my portfolio site! Explore the links above to see some of the things that I've been up to! Or, keep reading below to get to know me a little better! </p>
         <p>My name is Cole, and I enjoy creating websites, apps, and even games! I recently graduated (May 2021) with a Bachelor's degree in Computer Science. Between my coursework and personal projects, I've done a lot of programming in multiple languages, and have utilized a wide variety of tools, frameworks, and libraries. In the rest of my free time, I love hiking, playing guitar, and exploring new places!  </p>
     </div>
 }
