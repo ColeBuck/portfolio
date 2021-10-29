30b7eb800f3a047f5f3f903bf6879e4538769373
Cole Buck <67663402+ColeBuck@users.noreply.github.com>
2021-10-29 17:12:59 -0400

    add resume

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,7 @@
 import React from "react"
 import './Navbar.scss';
 import cole from "../../static/images/cole.jpg"
+import resume from "../../static/resume_public.pdf"
 
 export default function Navbar(props) {
     return <div class="navbar">
@@ -8,7 +9,7 @@ export default function Navbar(props) {
             <li><a id="name" href="/">Cole Buck</a></li>
             <li><a href="/projects" class={(props.active === "projects" ? "active" : "")}>Projects</a></li>
             <li><a href="/coursework" class={(props.active === "coursework" ? "active" : "")}>Coursework</a></li>
-            <li><a>Résumé</a></li>
+            <li><a href={resume} target="_">Résumé</a></li>
             <img src={cole} width="225px" alt="Cole"/>
         </ul>
     </div>
